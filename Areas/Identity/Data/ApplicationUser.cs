3890eef2af92b8b9d2792b39952fb733b2841fd4
hofmannicusrex <nickhofmann1989@hotmail.com>
2022-04-24 20:37:12 -0500

    Changed the size of the FirstName and LastName columns to nvarchar(20) from nvarchar(100).

--- Areas/Identity/Data/ApplicationUser.cs
+++ Areas/Identity/Data/ApplicationUser.cs
@@ -11,10 +11,10 @@ namespace BrassDragonArchive.Areas.Identity.Data
     public class ApplicationUser : IdentityUser
     {
         [PersonalData]
-        [Column(TypeName = "nvarchar(100)")]
+        [Column(TypeName = "nvarchar(20)")]
         public string FirstName { get; set; }
         [PersonalData]
-        [Column(TypeName = "nvarchar(100)")]
+        [Column(TypeName = "nvarchar(20)")]
         public string LastName { get; set; }
     }
 }
